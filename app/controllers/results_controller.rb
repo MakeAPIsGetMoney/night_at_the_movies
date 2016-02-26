0599cbd45f0ba31de05fb1aac1743e9b55735f36
Kate Studwell <katestudwell@gmail.com>
2016-02-26 14:08:58 -0500

    Add error checking to controller.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,11 @@
 class ResultsController < ApplicationController
   def show
-    @movie = MyFilm.new(params[:movie])
-    @song = Music.new(genre: params[:genre], year: @movie.year)
+    if params[:genre] == nil
+      render json: "Please pass in both a movie and a music genre!"
+    else
+      @movie = MyFilm.new(params[:movie])
+      @song = Music.new(genre: params[:genre], year: @movie.year)
+    end
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'results/index'
 
   get '/api/v1/search/:movie/:genre' => 'results#show'
+  get '/api/v1/search' => 'results#show'
+  get '/api/v1/search/:movie' => 'results#show'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
